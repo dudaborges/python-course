2da8911c4462d6bb9ad92017ae8633e87d46256c
dudaborges <duda.pborges92@gmail.com>
2023-09-11 22:40:02 -0300

    update string formatting

--- mudo-2/formatacao/color.py
+++ mudo-2/formatacao/color.py
@@ -1,5 +1,6 @@
-print('\033[1;31;40mOlá, mundo!')
-print('\033[1;31;40mOlá, mundo!\33[m')
+# print('\033[1;30;40mOlá, mundo!')
+print('\033[1;37;40mOlá, mundo!\33[m')
 # style: 0, 1, 4, 7
-# 3 -> text 4 -> background
-# o ao 7 -> white, red, green, yellow, blue, purple, cyan, gray
+# 3 -> text 
+# 4 -> background
+# o ao 7 -> 0 gray, 1 red, 2 green, 3 yellow, 4 purple, 5 pink, 6 blue, 7 white
