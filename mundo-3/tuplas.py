6c449a7901e97dc475f3ef235d964aedb75e978a
dudaborges <duda.pborges92@gmail.com>
2023-10-08 17:43:37 -0300

    refactor

--- mundo-3/tuplas.py
+++ mundo-3/tuplas.py
@@ -1,27 +1,26 @@
-#tuplas são imutáveis
-lanche = ('hamburguer','suco','pizza','pudim')
+# tuplas são imutáveis
+lanche = ('hamburguer', 'suco', 'pizza', 'pudim')
 
-#pelo index
-print("2 ", lanche[2])
+# pelo index
+print('2 ', lanche[2])
 
 # pega do elemento até o último
-print("1: ", lanche[1:])
+print('1: ', lanche[1:])
 
-#pega do início até o elemento
-print(":1", lanche[:3])
+# pega do início até o elemento
+print(':1', lanche[:3])
 
-#último elemento
-print("-1 ", lanche[-1])
+# último elemento
+print('-1 ', lanche[-1])
 
-#pega pelo fatiamento
-print("0:2", lanche[0:1])
+# pega pelo fatiamento
+print('0:2', lanche[0:1])
 
 for l in lanche:
     print(l)
 
-#ou
-
+# ou
 for i in range(0, len(lanche)):
     print(lanche[i])
 
-#para apagar uma tupla inteira: del(nome_tupla)
+# para apagar uma tupla inteira: del(nome_tupla)
